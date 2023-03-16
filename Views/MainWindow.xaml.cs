89e84f5fc0cbe0b1b5d381305ea49cb7f858d370
exe <igorwdowiczak@gmail.com>
2023-03-16 17:06:16 +0100

    :anchor: Transefered DataContext declaration to MainWindow class

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WeatherApp.ViewModel;
 
 namespace WeatherApp
 {
@@ -23,6 +24,8 @@ namespace WeatherApp
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new WeatherViewModel();
         }
     }
 }
